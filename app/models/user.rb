74d16e0b546a7f57aa7f28301c8b8dc18328adcf
sjburji <supreethjburji@yahoo.com>
2011-04-19 19:21:17 +0530

    oyc app push

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  has_many :authentications
+  
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessor :current_branch
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+
+  def apply_omniauth(omniauth)
+    self.email = omniauth['user_info']['email'] if email.blank?
+    self.username = omniauth['user_info']['name'] if username.blank?
+
+    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+  end
+
+  def password_required?
+    (authentications.empty? || !password.blank?) && super
+  end
+
+  def strata_employee?
+    email.gsub(/.*@/,'').split('.').include?('strata')
+  end
+
+  def subdomain
+    email.gsub(/.*@/,'').split('.')[0]
+  end
+
+  def frontoffice?
+    email.split('@')[0].downcase.eql?('frontoffice')
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OycApp::Application.config.secret_token = 'd47601987ca115b0154ada7e41ef9e1645b2a9352768724553211159a56e925176983d4ca26f3fe7af27c3ba83d4c9497d71df39be08ede8bd01c746c98aa502'
